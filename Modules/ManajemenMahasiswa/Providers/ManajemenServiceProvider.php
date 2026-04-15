b63b47ba2283ded16a54052700a27905210bee7f
Devarlo <devarlorazan05@gmail.com>
2026-04-15 23:36:39 +0700

    feat(Kemahasiswaan): Update controller Pengumuman & fix routing

--- Modules/ManajemenMahasiswa/Providers/ManajemenServiceProvider.php
+++ Modules/ManajemenMahasiswa/Providers/ManajemenServiceProvider.php
@@ -30,8 +30,8 @@ class ManajemenMahasiswaServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
-        $this->loadRoutesFrom(__DIR__ . '/routes/api.php');
-        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
+        $this->loadMigrationsFrom(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'database' . DIRECTORY_SEPARATOR . 'migrations');
+        $this->loadRoutesFrom(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'routes' . DIRECTORY_SEPARATOR . 'api.php');
+        $this->loadRoutesFrom(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'routes' . DIRECTORY_SEPARATOR . 'web.php');
     }
 }
