15aafa65cab917c0e3ce3f3b31aa476626b191c4
SyhbnHtb <165374759+SyhbnHtb@users.noreply.github.com>
2026-04-16 19:21:39 +0700

    Feat(Kemahasiswaan): Add Menu Sidebar Layanan Pengaduan

--- Modules/ManajemenMahasiswa/resources/views/components/ui/sidebar-admin.blade.php
+++ Modules/ManajemenMahasiswa/resources/views/components/ui/sidebar-admin.blade.php
@@ -29,10 +29,14 @@
             </x-slot:iconSlot>
         </x-manajemenmahasiswa::ui.sidebar-item>
 
-        <x-manajemenmahasiswa::ui.sidebar-item route="#" label="Direktori Mahasiswa">
+        <x-manajemenmahasiswa::ui.sidebar-item route="#">
             <x-slot:iconSlot>
                 <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M22 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
             </x-slot:iconSlot>
+            <div class="d-flex justify-content-between align-items-center w-100" style="flex-grow: 1;">
+                <span>Direktori Mahasiswa</span>
+                <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m9 18 6-6-6-6"/></svg>
+            </div>
         </x-manajemenmahasiswa::ui.sidebar-item>
 
         <x-manajemenmahasiswa::ui.sidebar-item route="#" label="Kegiatan">
@@ -45,6 +49,12 @@
             routeName="manajemenmahasiswa.forum" label="Forum Diskusi">
             <x-slot:iconSlot>
                 <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path></svg>
+            </x-slot:iconSlot>
+        </x-manajemenmahasiswa::ui.sidebar-item>
+
+        <x-manajemenmahasiswa::ui.sidebar-item route="#" label="Layanan Pengaduan">
+            <x-slot:iconSlot>
+                <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path></svg>
             </x-slot:iconSlot>
         </x-manajemenmahasiswa::ui.sidebar-item>
 
